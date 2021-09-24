fb96ee51545392571d32cd8424e8bf8ee1751143
Emmanuel Chargoy <charger513@hotmail.com>
2021-09-23 19:48:15 -0500

    It changes the statusbar brightness

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/injection_container.dart' as di;
 import 'package:movies_app/movies/presentation/bloc/playing_now/playing_now_bloc.dart';
@@ -9,6 +10,14 @@ import 'package:movies_app/movies/presentation/pages/home_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    // systemNavigationBarColor: Colors.blue, // navigation bar color
+    // statusBarColor: Colors.pink, // status bar color
+    statusBarBrightness: Brightness.dark, //status bar brigtness
+    statusBarIconBrightness: Brightness.dark, //status barIcon Brightness
+    // systemNavigationBarDividerColor: Colors.greenAccent,//Navigation bar divider color
+    // systemNavigationBarIconBrightness: Brightness.light, //navigation bar icon
+  ));
   runApp(App());
 }
 
@@ -22,6 +31,7 @@ class App extends StatelessWidget {
         BlocProvider(create: (_) => di.sl<UpcomingBloc>()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Movies App',
         theme: ThemeData(
           brightness: Brightness.dark,
